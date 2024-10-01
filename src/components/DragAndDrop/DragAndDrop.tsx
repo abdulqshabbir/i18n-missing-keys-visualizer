0ffa137ad217060561bc1565e0784dd294975411
Abdul Shabbir <abdulqshabbir@gmail.com>
2024-10-01 08:25:21 +0800

    refactor: :recycle: move missing key functions to utils folder

--- src/components/DragAndDrop/DragAndDrop.tsx
+++ src/components/DragAndDrop/DragAndDrop.tsx
@@ -2,65 +2,8 @@ import { Check, Drum, FolderUp } from "lucide-react"
 import { useCallback } from "react"
 import { useDropzone } from "react-dropzone"
 import { type DragAndDropProps } from "@/types"
+import { findNumberOfFilledOrMissingKeysRecurse, findMissingKeysRecurse, isJsonString } from "../../utils"
 
-function isJsonString(str: string) {
-  try {
-    JSON.parse(str)
-  } catch (e) {
-    return false
-  }
-  return true
-}
-
-function findMissingKeysRecurse(
-  obj: Record<string, unknown>,
-  missingKeys: string[] = [],
-  prefix = "",
-): string[] {
-  const keys = Object.keys(obj)
-
-  for (const key of keys) {
-    // This is a leaf node
-    if (typeof obj[key] === "string") {
-      if (obj[key] === "") {
-        missingKeys.push(key)
-      }
-    } else {
-      // This is a nested object
-      findMissingKeysRecurse(
-        obj[key] as Record<string, unknown>,
-        missingKeys,
-        !prefix ? key : `${prefix}.${key}`,
-      )
-    }
-  }
-
-  return missingKeys
-}
-
-function findNumberOfFilledOrMissingKeysRecurse(
-  obj: Record<string, unknown>,
-  count = { filledKeys: 0, missingKeys: 0 },
-) {
-  const keys = Object.keys(obj)
-  for (const key of keys) {
-    // This is a leaf nod
-    if (typeof obj[key] === "string") {
-      if (obj[key] !== "") {
-        count.filledKeys = count.filledKeys + 1
-      } else {
-        count.missingKeys = count.missingKeys + 1
-      }
-    } else {
-      // This is a nested object
-      findNumberOfFilledOrMissingKeysRecurse(
-        obj[key] as Record<string, unknown>,
-        count,
-      )
-    }
-  }
-  return count
-}
 function DragAndDrop({
   isDoneParsing,
   setNumberOfMissingKeys,

fa205d78a3bac51819e0d79a11aa24f8d4edcc2d
Abdul Shabbir <abdulqshabbir@gmail.com>
2024-10-31 11:21:08 +0800

    fix: :bug: git ignorecase false

---
+++ src/components/footer/footer.tsx
@@ -0,0 +1,15 @@
+function Footer() {
+  return (
+    <footer className="flex flex-col items-center justify-center text-primary">
+      <div className="text-md">Missing Keys Visualizer</div>
+      <div className="text-sm text-gray-500">
+        Built with ❤️ by{" "}
+        <a target="_blank" href="https://github.com/abdulqshabbir">
+          Abdul Shabbir
+        </a>
+      </div>
+    </footer>
+  )
+}
+
+export { Footer }

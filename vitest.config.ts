1516adedfde7692ec3c048d232e2dba0a9b697a2
Abdul Shabbir <abdulqshabbir@gmail.com>
2024-10-31 11:10:31 +0800

    fix: :bug: fix

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,14 +1,12 @@
 import { defineConfig } from "vitest/config"
 import react from "@vitejs/plugin-react"
 import tsConfigPaths from "vite-tsconfig-paths"
-import path from "path"
+import { resolve } from "path"
 
 export default defineConfig({
   plugins: [react(), tsConfigPaths()],
   resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
+    alias: [{ find: "@", replacement: resolve(__dirname, "./src") }],
   },
   test: {
     globals: true,

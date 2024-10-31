33dc91d6b66795554faed33c15df86a778e5d629
Abdul Shabbir <abdulqshabbir@gmail.com>
2024-10-31 11:06:14 +0800

    fix: :bug: try to update vitest import alias

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from "vitest/config"
 import react from "@vitejs/plugin-react"
 import tsConfigPaths from "vite-tsconfig-paths"
+import path from "path"
 
 export default defineConfig({
   plugins: [react(), tsConfigPaths()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   test: {
     globals: true,
     environment: "jsdom",
